b09f833e3b5aef71128813ab277729b2117ef057
mquanaazi <olwethumalumla31@gmail.com>
2024-05-17 16:11:45 +0200

    added a readme file

---
+++ script/calculator.js
@@ -0,0 +1,34 @@
+
+let button7=document.querySelectorAll('button')[0]
+let button8=document.querySelectorAll('button')[1]
+let button9=document.querySelectorAll('button')[2]
+let buttona=document.querySelector('#devides')
+
+let button4=document.querySelectorAll('button')[4]
+let button5=document.querySelectorAll('button')[5]
+let button6=document.querySelectorAll('button')[6]
+let buttonmm=document.querySelector('#minuses')
+
+let button1=document.querySelectorAll('button')[8]
+let button2=document.querySelectorAll('button')[9]
+let button3=document.querySelectorAll('button')[10]
+let buttonm=document.querySelector('#multiplies')
+
+let button0=document.querySelectorAll('button')[12]
+let buttond=document.querySelector('#comma')
+let buttonp=document.querySelector('#addition')
+let buttone=document.querySelectorAll('#equals')
+let buttonc=document.querySelector('#clears')
+let buttons=document.querySelectorAll('button')
+let input=document.querySelector('#display')
+
+function add(value1,valu2) {
+  input.value=value1+valu2
+  return
+}
+buttonp=addEventListener('CLICK'(add))
+for (const button of buttons) {
+  function add() {
+    input.value=add
+  }
+}
